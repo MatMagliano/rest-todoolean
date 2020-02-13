24ac38603f2aa06db7d70fda97faa44b9f3773d3
Matteo Magliano <matteo.magliano01@gmail.com>
2020-02-13 16:44:00 +0100

    funzione print

--- js/script.js
+++ js/script.js
@@ -17,18 +17,35 @@ getCall();
 
 //----------- FUNZIONI --------------
 
+
 function getCall() {
   $.ajax(
 {
 url: "http://157.230.17.132:3022/todos",
 method: "GET",
 success: function (data) {
-  var todo = data;
-  console.log(todo);
+  var todos = data;
+  console.log(todos);
+  printAll(todos);
+
 },
 error: function (richiesta, stato, errori) {
 alert("E' avvenuto un errore. " + errore);
 }
 }
 );
-}
+} // FINE GET ALL
+//-------------------------------------
+function printAll(array) {
+  var source = $("#todo-template").html();
+  var template = Handlebars.compile(source);
+  for (var i = 0; i < array.length; i++) {
+    var list = array[i];
+    var context = {
+      text: list.text,
+      id: list.text
+  }
+  var html = template(context);
+  $('.todo-ul').append(html);
+  };
+} // FINE PRINT ALL
