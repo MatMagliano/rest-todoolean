999b4485c23c9668748b42271f9370299c5d0add
Matteo Magliano <matteo.magliano01@gmail.com>
2020-02-13 16:10:19 +0100

    log lista iniziale

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
+// Todoolean
+// Creiamo una app che permette di inserire e cancellare dei todos
+// in una lista utilizzando la API boolean per fare operazioni CRUD.
+
+
+
+
+
+
 $(document).ready(function functionName() {
   console.log('ciao');
+
+getCall();
+
 }); //FINE READY
+
+
+//----------- FUNZIONI --------------
+function getCall() {
+  $.ajax(
+{
+url: "http://157.230.17.132:3022/todos",
+method: "GET",
+success: function (data) {
+  var todo = data;
+  console.log(todo);
+},
+error: function (richiesta, stato, errori) {
+alert("E' avvenuto un errore. " + errore);
+}
+}
+);
+}
