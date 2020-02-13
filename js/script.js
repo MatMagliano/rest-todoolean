2332cf7a26c0cb3058cea79bf9f993d1b339da1a
Matteo Magliano <matteo.magliano01@gmail.com>
2020-02-13 19:04:14 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -13,54 +13,17 @@ $('.add-button').click(function() {
   var newTodo = $('.add-input').val();
   addTodo(newTodo);
 });
+$(document).on('click', '.delete', function() {
+  var deleteThis = $(this);
+  var idTodo = deleteThis.parent().attr('data-todo');
 
+  deleteTodo(idTodo);
+});
 
 }); //FINE READY
 
 
 //----------- FUNZIONI --------------
-
-
-function getCall() {
-  $.ajax(
-{
-url: "http://157.230.17.132:3022/todos",
-method: "GET",
-success: function (data) {
-  var todos = data;
-
-  $('.todo-ul').html('');
-  printAll(todos);
-
-},
-error: function (richiesta, stato, errori) {
-alert("E' avvenuto un errore. " + errore);
-}
-});
-} // FINE GET ALL
-//-------------------------------------
-
-function addTodo(addInput) {
-  $.ajax(
-{
-url: "http://157.230.17.132:3022/todos",
-method: "POST",
-data: {
-  text: addInput
-},
-success: function (data) {
-  $('.todo-ul').html('');
-  printAll(addInput);
-
-},
-error: function (richiesta, stato, errori) {
-alert("E' avvenuto un errore. " + errore);
-}
-});
-} // FINE ADD TODO
-//-------------------------------------
-
-
 
 function printAll(array) {
   var source = $("#todo-template").html();
@@ -70,9 +33,58 @@ function printAll(array) {
     var context = {
       text: list.text,
       id: list.id
-  }
-  var html = template(context);
-  $('.todo-ul').append(html);
+    }
+    var html = template(context);
+    $('.todo-ul').append(html);
   };
 } // FINE PRINT ALL
+//-------------------------------------
+
+function getCall() {
+  $.ajax({
+    url: "http://157.230.17.132:3022/todos",
+    method: "GET",
+    success: function (data) {
+      var todos = data;
+      $('.todo-ul').html('');
+      printAll(todos);
+    },
+    error: function (richiesta, stato, errori) {
+      alert("E' avvenuto un errore. " + errore);
+    }
+  });
+} // FINE GET ALL
+//-------------------------------------
+
+function addTodo(addInput) {
+  $.ajax({
+    url: "http://157.230.17.132:3022/todos",
+    method: "POST",
+    data: {
+  text: addInput
+},
+    success: function (data) {
+    $('.todo-ul').html('');
+    getCall();
+    },
+    error: function (richiesta, stato, errori) {
+      alert("E' avvenuto un errore. " + errore);
+    }
+  });
+} // FINE ADD TODO
+//-------------------------------------
+
+function deleteTodo(id) {
+  $.ajax({
+    url: "http://157.230.17.132:3022/todos/" + id,
+    method: "DELETE",
+    success: function (data) {
+      $('.todo-ul').html('');
+      getCall();
+    },
+    error: function (richiesta, stato, errori) {
+      alert("E' avvenuto un errore. " + errore);
+    }
+  });
+} // FINE DELETE
 //-------------------------------------
